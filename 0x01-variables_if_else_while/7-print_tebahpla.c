708dd9838058633dd5ad77813eafec624656eae1
myname <moreenkinyua4@gmail.com>
2023-08-11 08:50:28 -0700

    Number 18

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the lowercase alphabet in reverse.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
-char alphabet;
-for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
-		{
-			putchar(alphabet);
-		}
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+
 	putchar('\n');
 
 	return (0);
