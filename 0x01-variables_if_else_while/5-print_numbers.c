7f2ab397e39131e6569a5f063d3b41147d06e807
myname <moreenkinyua4@gmail.com>
2023-08-11 08:25:08 -0700

    number 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int y;
+
+	for (y = 0; y <= 10; y++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
