8fb9f334da68e9daca8c8ebe17ead794f7cb6a6e
myname <moreenkinyua4@gmail.com>
2023-08-17 01:56:39 -0700

    isdigit and mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Miltiples two integers.
+ * @a: Value to be checked
+ * @b: Integer to be checked
+ * Return: 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+	return (0);
+}
