9417c7bd353c16c0be4dc8b35590d0c8e06df2e7
myname <moreenkinyua4@gmail.com>
2023-08-17 08:50:53 -0700

    Print numbers without long

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *@n: the number that is to be printed.
+ * Return: 0 (Success)
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+_putchar(n % 10 + '0');
+}
