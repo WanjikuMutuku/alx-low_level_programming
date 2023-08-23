81d60242e12e7e95b81cfd266e0c8c1ab5974caa
myname <moreenkinyua4@gmail.com>
2023-08-23 12:33:10 -0700

    Capitalize strinf

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: value to be capitalised.
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char * str)
+{
+	int i;
+	
+	i = 0;
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] -= 32;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i-1] == 32)
+		{
+			if(str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] -= 32;
+			} 
+		}
+	}
+	return (str);
+}
