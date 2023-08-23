198a191bc7c3fbc3a9eb8d53583ab5db37fa5b8c
myname <moreenkinyua4@gmail.com>
2023-08-23 14:05:05 -0700

    ROT 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#define ROT 13
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: value to be encoded.
+ *
+ * Return: always 0.
+ */
+
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = rot[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char * str)
 	i = 0;
 	if (str[0] >= 97 && str[0] <= 123)
 	{
-		str[0] -= 32;
+		str[i] = toupper(str[i]);
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -24,13 +24,9 @@ char *cap_string(char * str)
 		{
 			if(str[i] >= 97 && str[i] <= 123)
 			{
-				str[i] -= 32;
+				str[i] = toupper(str[i]);
 			} 
 		}
-	}
-	if (str[i] != ' ')
-	{
-		str[i] -= 32;
 	}
 	return (str);
 }
