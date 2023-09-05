b69807d9f1608c54382b1d0678a6ec8315698a7d
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-09-05 00:13:52 -0700

    concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 		len += strlen(av[i]) + 1;
-	str = malloc(len * sizeof(char));
+	str = malloc(len * sizeof(char) + 1);
 	if (str == NULL)
 		return (NULL);
 	str[0] = '\0';
