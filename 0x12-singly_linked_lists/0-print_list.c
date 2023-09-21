3e33b0a00cba96257aaabffb84bff4a13c69644b
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-09-21 10:50:19 -0700

    prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str != NULL)
 		{
-			printf("[%lu] %s\n", strlen(h->str), h->str);
+			printf("[%lu] %s\n", strlen(h->len), h->str);
 		}
 		else
 		{
