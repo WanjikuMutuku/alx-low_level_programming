b6387ff04a03fce53e75d9cb2ae6532541c1a5dc
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-09-21 11:09:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t len = 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
-		len++;
 		h = h->next;
+		s++;
 	}
-	return (len);
+
+	return (s);
 }
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t.
+ * @h: pointe to the list_t
+ *
+ * Return: elements linked.
+ */
+size_t list_len(const list_t *h)
+{
+
