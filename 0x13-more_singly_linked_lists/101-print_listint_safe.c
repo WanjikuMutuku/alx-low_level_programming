87e1fddc3f1d88f03616c2ae27270ba892fc8e11
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-09-26 08:30:24 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,7 +13,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current = head;
 	size_t count = 0;
 
-	while (current)
+	while (current != NULL)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
