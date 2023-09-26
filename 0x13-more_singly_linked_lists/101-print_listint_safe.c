0c3c5226d5a20beb25eeba7a27c36a22f9ae119f
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-09-26 08:17:53 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,18 +10,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_count = 0;
 	const listint_t *current = head;
+	size_t count = 0;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		if (current->next >= current)
+		{
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			break;
+		}
 		current = current->next;
-		node_count++;
 	}
-	if (ferror(stdout))
-	{
-		exit(98);
-	}
-	return (node_count);
+
+	return (count);
 }
