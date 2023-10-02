35e0bc48bd3fbcde89f5c3b622eed5da68307c55
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-10-02 02:14:56 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,22 +12,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filedesc;
-	ssize_t byteswritten = write(filedesc, text_content, strlen(text_content));
+	ssize_t b_written = 0;
+	int fd;
 
-	filedesc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		if (byteswritten == -1)
-		{
-			close(filedesc);
-			return (-1);
-		}
-	}
-	close(filedesc);
-	return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+		b_written = write(fd, text_content, strlen(text_content));
+
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
+	return (1);
 }
