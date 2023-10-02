022d16d4acf48751d61d77b9b919584e20a6aaa9
WanjikuMutuku <moreenkinyua4@gmail.com>
2023-10-02 03:11:40 -0700

    number 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,21 +29,23 @@ ssize_t _strlen(const char *str)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int filedesc = open(filename, O_WRONLY | O_APPEND);
-	ssize_t byteswritten = write(filedesc, text_content, strlen(text_content));
+	ssize_t b_written = 0;
+	int fd;
 
 	if (!filename)
-	{
 		return (-1);
-	}
-	if (filedesc < 0)
-	{
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
 		return (-1);
-	}
-	close(filedesc);
-	if (byteswritten < 0)
-	{
+
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+	if (b_written < 0)
 		return (-1);
-	}
 	return (1);
 }
