0e3b160088f12690e95aacd0748c4374f7b0309b
wanjikumutuku <moreenkinyua4@gmail.com>
2023-10-30 11:03:59 -0700

    Corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,42 @@
-#include "main.h"
-#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 #include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
 /**
- * read_textfile - reads a text file and prints it to standard output.
- * @filename: name of the file to print
- * @letters: number of letters it should read and print
+ * read_textfile - that reads a text file and prints
+ * @filename: name of the file
+ * @letters: size letters
  *
- * Return: actual number of letters it could read and print
+ * Return: the actual number of letters, 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int desc = open(filename, O_RDONLY);
-	char *buffer = malloc(letters);
-	ssize_t bytes_read = read(desc, buffer, letters);
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	ssize_t file, let, w;
+	char *text;
 
-	if (filename == NULL || buffer == NULL)
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
 	{
+		free(text);
 		return (0);
 	}
-	if (desc == -1)
-		return (0);
-	if (buffer == NULL)
-	{
-		close(desc);
-		return (0);
-	}
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(desc);
-		return (0);
-	}
-	free(buffer);
-	close(desc);
-	if (bytes_written == bytes_read)
-	{
-		return (bytes_written);
-	}
-	else
-	{
-		return (0);
-	}
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
